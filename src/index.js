dead51a0ccafd4fdba05d52d72c3f4895b2b4965
fellipebastos <fellipebastoswm@gmail.com>
2019-07-14 20:44:59 -0300

    first commitgit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
+import PropTypes from "prop-types";
+
+class Button extends Component {
+  static defaultProps = {
+    children: "Salvar"
+  };
+
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.string
+  };
+
+  render() {
+    return (
+      <button type="button" onClick={this.props.onClick}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    counter: 0
+  };
+
+  handleClick = () => {
+    this.setState({ counter: (this.state.counter += 1) });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello Rocketseat!</h1>
+        <h2>{this.state.counter}</h2>
+        <Button onClick={this.handleClick}>Enviar</Button>
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
